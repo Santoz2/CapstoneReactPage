05bb97e75815f28ff2dd4de3702adfc58741d4f9
Zaijen Santos <santoz2@uw.edu>
2020-02-16 15:41:37 -0800

    Make react page the main page

---
+++ precache-manifest.19c519926c2ab81382ebb436335568d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e269a10064903df3d2db1ea1bc9dca0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d2e69b78d22e87db218",
+    "url": "/static/css/main.3a12b05f.chunk.css"
+  },
+  {
+    "revision": "892de7a8198fdfcc2f9e",
+    "url": "/static/js/2.dc302288.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.dc302288.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d2e69b78d22e87db218",
+    "url": "/static/js/main.3495da42.chunk.js"
+  },
+  {
+    "revision": "cea62d2cb4df84fb0bf2",
+    "url": "/static/js/runtime-main.5ee9a5f7.js"
+  },
+  {
+    "revision": "30581fd02a3781bc3f9b3dd046498f7d",
+    "url": "/static/media/kevBanner.30581fd0.jpg"
+  }
+]);
